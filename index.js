31573d121c49bfd502fc8216c26b111f79e59d25
Michael Wittig <post@michaelwittig.info>
2016-06-10 08:51:31 +0200

    implemented dynamodb writes

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 "use strict";
 
+var AWS = require("aws-sdk");
+var dynamodb = new AWS.DynamoDB();
+
 exports.handler = function(event, context, cb) {
   console.log(JSON.stringify(event));
-  cb({});
+  dynamodb.putItem({
+    "Item": {
+      "id": {
+        "S": event.id
+      },
+      "latitude": {
+        "N": event.latitude.toString()
+      },
+      "longitude": {
+        "N": event.longitude.toString()
+      }
+    },
+    "TableName": "location"
+  }, function(err) {
+    if (err) {
+      cb(err);
+    } else {
+      cb(null, {});
+    }
+  });
 };
