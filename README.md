dba74faf50823a53aa82d766b149d2adda5d434e
Michael Wittig <post@michaelwittig.info>
2016-06-10 15:33:12 +0200

    added get action

---
+++ README.md
@@ -0,0 +1,1 @@
+curl -vvv -X PUT -d '' -H "Authorization: 123" "https://cv89dvpu1a.execute-api.us-east-1.amazonaws.com/v1/location/123"
--- index.js
+++ index.js
@@ -5,24 +5,53 @@ var dynamodb = new AWS.DynamoDB();
 
 exports.handler = function(event, context, cb) {
   console.log(JSON.stringify(event));
-  dynamodb.putItem({
-    "Item": {
-      "id": {
-        "S": event.id
+  if (event.action === "get") {
+    dynamodb.getItem({
+      "Key": {
+        "id": {
+          "S": event.id
+        }
       },
-      "latitude": {
-        "N": event.body.latitude.toString()
-      },
-      "longitude": {
-        "N": event.body.longitude.toString()
+      "TableName": "location"
+    }, function(err, data) {
+      if (err) {
+        cb(err);
+      } else {
+        if (data.Item === undefined) {
+          cb(new Error("ServiceErrorNotFound"));
+        } else {
+          console.log(JSON.stringify(data.Item));
+          cb(null, {
+            "status": 200,
+            "body": {
+              "id": data.Item.id.S,
+              "latitude": data.Item.latitude.N,
+              "longitude": data.Item.longitude.N
+            }
+          });
+        }
       }
-    },
-    "TableName": "location"
-  }, function(err) {
-    if (err) {
-      cb(err);
-    } else {
-      cb(null, {});
-    }
-  });
+    });
+  } else if (event.action === "put") {
+    dynamodb.putItem({
+      "Item": {
+        "id": {
+          "S": event.id
+        },
+        "latitude": {
+          "N": event.body.latitude.toString()
+        },
+        "longitude": {
+          "N": event.body.longitude.toString()
+        }
+      },
+      "TableName": "location"
+    }, function(err) {
+      if (err) {
+        cb(err);
+      } else {
+        cb(null, {});
+      }
+    });
+  }
 };
